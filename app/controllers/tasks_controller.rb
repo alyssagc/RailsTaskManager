a45c4b193f7946d2dc61ec03a6d9763ab279bb01
Alyssa Coelho <alyssagcoelho@gmail.com>
2021-11-16 17:15:26 -0300

    ex incompleto

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,39 @@
+class TasksController < ApplicationController
+  def list
+    @tasks = Task.all
+  end
+  def display
+    @task = Task.find(params[:id])
+  end
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(tasks_params)
+    @task.save
+    redirect_to tasks_path(@tasks)
+  end
+
+  def edit
+     @task = Task.find(params[:id])
+  end
+
+   def update
+    @task = Task.find(params[:id])
+    @task.update(tasks_params)
+    redirect_to tasks_path(@tasks)
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path
+  end
+
+  private
+
+  def tasks_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
+end
